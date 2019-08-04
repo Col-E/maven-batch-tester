8e83f3713c11c5b8022fd6c8065140bcbeb69cf5
Col-E <mcoley2@gmu.edu>
2019-08-04 01:28:31 -0400

    Change pom swap logic, assumes property set

--- src/main/java/me/coley/autotest/TestInvokeThread.java
+++ src/main/java/me/coley/autotest/TestInvokeThread.java
@@ -3,13 +3,7 @@ package me.coley.autotest;
 import org.apache.commons.io.FileUtils;
 import org.apache.maven.shared.invoker.*;
 import org.pmw.tinylog.Logger;
-import org.w3c.dom.*;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.transform.*;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
 import java.io.File;
 import java.nio.file.*;
 import java.util.*;
@@ -17,7 +11,6 @@ import java.util.concurrent.Callable;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import java.util.stream.Collectors;
 
 import static java.nio.charset.Charset.defaultCharset;
 
@@ -86,11 +79,9 @@ public class TestInvokeThread implements Callable<TestResultGroups> {
 		this.phase = phase;
 		this.rootPom = new File(dir, "pom.xml");
 		String name = dir.getName();
-		if (name.contains("-master")) {
+		if (name.contains("-master"))
 			name = name.substring(0, name.length()-7);
-		}
 		this.name = name;
-
 	}
 
 	@Override
@@ -99,41 +90,35 @@ public class TestInvokeThread implements Callable<TestResultGroups> {
 		List<TestResults> standard = new ArrayList<>();
 		List<TestResults> forkscript = new ArrayList<>();
 		List<TestResults> custom = new ArrayList<>();
-		// Initial compile
-		compile();
 		// Run tests with different plugin versions
 		// - Standard current release
 		// - Our custom release
 		// - Forkscript release
 		//
 		// List of pom files to update (since some projects are modular)
-		List<File> poms = Files.walk(dir.toPath())
-				.filter(f -> f.toString().endsWith("pom.xml"))
-				.map(Path::toFile)
-				.sorted()
-				.collect(Collectors.toList());
+		File pom = new File(dir, "pom.xml");
 		switch(phase) {
 			case "STANDARD":
-				exec(poms, standard, "3.0.0-M3", "Standard");
+				exec(pom, standard, "3.0.0-M3", "Standard");
 				break;
 			case "CUSTOM":
-				exec(poms, custom, "3.0.0-SNAPSHOT", "Custom");
+				exec(pom, custom, "3.0.0-SNAPSHOT", "Custom");
 				break;
 			case "FORKSCRIPT":
-				exec(poms, forkscript, "2.21.0", "Forkscript");
+				exec(pom, forkscript, "2.21.0", "Forkscript");
 				break;
 			default:
-				exec(poms, standard, "3.0.0-M3", "Standard");
-				exec(poms, custom, "3.0.0-SNAPSHOT", "Custom");
-				exec(poms, forkscript, "2.21.0", "Forkscript");
+				exec(pom, standard, "3.0.0-M3", "Standard");
+				exec(pom, custom, "3.0.0-SNAPSHOT", "Custom");
+				exec(pom, forkscript, "2.21.0", "Forkscript");
 				break;
 		}
 		return new TestResultGroups(name, standard, forkscript, custom);
 	}
 
-	private void exec(List<File> poms, List<TestResults> results, String version, String phase)
+	private void exec(File pom, List<TestResults> results, String version, String phase)
 			throws Exception {
-		setupPoms(version, poms);
+		setupPom(version, pom);
 		int iteration = 0;
 		int collections = 0;
 		while(true) {
@@ -153,152 +138,14 @@ public class TestInvokeThread implements Callable<TestResultGroups> {
 		}
 	}
 
-	private void setupPoms(String versionStr, Collection<File> poms) throws Exception {
-		for(File pom : poms) {
-			setupPom(versionStr, pom);
-		}
-	}
-
 	private void setupPom(String versionStr, File pom) throws Exception {
-		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-		DocumentBuilder db = dbf.newDocumentBuilder();
-		Document doc = db.parse(pom);
-		// Nodes to update
-		Node surefire = null, version = null, configuration = null;
-		boolean foundSurefire = false;
-		// Iterate over plugins
-		NodeList plugins = doc.getElementsByTagName("plugin");
-		for (int i = 0; i < plugins.getLength(); i++) {
-			Node plugin = plugins.item(i);
-			NodeList attributes = plugin.getChildNodes();
-			// Get version and config version of the surefire plugin
-			// - If the plugin is not surefire, we skip this iteration of i.
-			for (int j = 0; j < attributes.getLength(); j++) {
-				Node attribute = attributes.item(j);
-				if (attribute.getNodeName().equals("artifactId") && attribute.getTextContent().equals("maven-surefire-plugin")) {
-					surefire = plugin;
-					foundSurefire = true;
-				} else if (version == null && foundSurefire && attribute.getParentNode().equals(plugin) && attribute.getNodeName().equals("version")) {
-					version = attribute;
-				}  else if (configuration == null && foundSurefire && attribute.getParentNode().equals(plugin) && attribute.getNodeName().equals("configuration")) {
-					configuration = attribute;
-				}
-			}
-			if (foundSurefire)
-				break;
-		}
-		if (!foundSurefire) {
-			if (pom.equals(rootPom)) {
-				// The root MUST specify the plugin
-				Logger.warn("Failed to find surefire plugin in \"{}\" - inserting plugin and restarting", name);
-				// Get <build>
-				NodeList builds = doc.getElementsByTagName("build");
-				if (builds.getLength() > 1) {
-					throw new IllegalStateException("Can't determine which <build> to insert maven-surefire-plugin into");
-				}
-				NodeList buildChildren = builds.item(0).getChildNodes();
-				// Get <build><plugins>
-				Node buildPlugins = null;
-				for (int i = 0; i < buildChildren.getLength(); i++) {
-					Node child = buildChildren.item(i);
-					if (child.getNodeName().equals("plugins")) {
-						buildPlugins = child;
-						break;
-					}
-				}
-				// Insert maven-surefire-plugin
-				surefire =  doc.createElement("plugin");
-				Node sfGroup =  doc.createElement("groupId");
-				Node sfArtifact =  doc.createElement("artifactId");
-				version =  doc.createElement("version");
-				sfGroup.setTextContent("org.apache.maven.plugins");
-				sfArtifact.setTextContent("maven-surefire-plugin");
-				version.setTextContent(versionStr);
-				surefire.appendChild(sfGroup);
-				surefire.appendChild(sfArtifact);
-				surefire.appendChild(version);
-				buildPlugins.appendChild(surefire);
-			} else {
-				// subprojects will defer to the root
-				return;
-			}
-		}
-		// Update version
-		if (version == null)  {
-			version = doc.createElement("version");
-			version.appendChild(doc.createTextNode(versionStr));
-			surefire.appendChild(version);
-		} else {
-			version.setTextContent(versionStr);
-		}
-		// Validate config
-		Node forkCount = null, reuseForks = null;
-		if (configuration == null) {
-			// Create the config attributes if needed
-			configuration = doc.createElement("configuration");
-			configuration.appendChild(forkCount = doc.createElement("forkCount"));
-			configuration.appendChild(reuseForks = doc.createElement("reuseForks"));
-			surefire.appendChild(configuration);
-		} else {
-			// Fetch the config attributes
-			NodeList attributes = configuration.getChildNodes();
-			for(int i = 0; i < attributes.getLength(); i++) {
-				Node attribute = attributes.item(i);
-				if(attribute.getNodeName().equals("forkCount")) {
-					forkCount = attribute;
-				} else if(attribute.getNodeName().equals("reuseForks")) {
-					reuseForks = attribute;
-				}
-			}
-			// Add if the attributes don't exist
-			if(forkCount == null) {
-				configuration.appendChild(forkCount = doc.createElement("forkCount"));
-			}
-			if(reuseForks == null) {
-				configuration.appendChild(reuseForks = doc.createElement("reuseForks"));
-			}
-		}
-		// Set the desired config
-		forkCount.setTextContent("1");
-		reuseForks.setTextContent("false");
-		// Save changes
-		Transformer transformer = TransformerFactory.newInstance().newTransformer();
-		Result output = new StreamResult(pom);
-		Source input = new DOMSource(doc);
-		transformer.transform(input, output);
-	}
-
-	private void compile() throws Exception {
-		// Log the compile output so that if it fails we can get the reason
-		StringBuilder log = new StringBuilder();
-		// Request to setup
-		InvocationRequest setup = new DefaultInvocationRequest();
-		setup.setPomFile(rootPom);
-		setup.setGoals(Arrays.asList("clean", "install"));
-		setup.setMavenOpts(INVOKE_OPTS + " -Dmaven.test.skip=true");
-		setup.setTimeoutInSeconds(TIMEOUT_SECONDS);
-			setup.setOutputHandler(line -> {
-				log.append(line + "\n");
-				if (emitMvnLogging) {
-					Logger.trace(line);
-				}
-			});
-		// Run setup compilation if target directory does not exist
-		File target = new File(dir, "target");
-		if (!target.exists()) {
-			Logger.info("Compiling \"{}\"", name);
-			InvocationResult res = invoker.execute(setup);
-			if(res.getExitCode() != 0) {
-				FileUtils.write(new File("build-fail-log.txt"), log.toString(), "UTF-8");
-				throw new IllegalStateException("Compile invoke failed.", res.getExecutionException());
-			}
-			// Just make the dummy folder so we don't try to compile again and waste time
-			if (!target.exists()) {
-				target.mkdir();
-			}
-		} else {
-			Logger.info("Skipping compilation for \"{}\", already has \"target\"", name);
-		}
+		String pomText = FileUtils.readFileToString(pom, "UTF-8");
+		int start = pomText.indexOf("<MSPV>");
+		int end = pomText.indexOf("</MSPV>") + 7;
+		String pre = pomText.substring(0, start);
+		String post = pomText.substring(end);
+		String newPom = pre + "<MSPV>" + versionStr + "</MSPV>" + post;
+		FileUtils.write(pom, newPom, "UTF-8");
 	}
 
 	private TestResults runTests(int run, String version) throws Exception {
@@ -354,9 +201,7 @@ public class TestInvokeThread implements Callable<TestResultGroups> {
 		boolean failed = res.getExitCode() != 0;
 		if(failed && killOnFail) {
 			Logger.error(res.getExecutionException(), "Test invoke failed.");
-			if (killOnFail) {
-				throw new IllegalStateException("Test invoke failed.", res.getExecutionException());
-			}
+			throw new IllegalStateException("Test invoke failed.", res.getExecutionException());
 		}
 		// Fetch results
 		File phaseLog = new File(dir, "maven.build.log");
